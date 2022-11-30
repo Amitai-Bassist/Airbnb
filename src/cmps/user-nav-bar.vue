e8962a2d172564852ac72841f8c79e38249dd656
Amitai Bassist <131amitaibasi@gmail.com>
2022-11-30 11:01:37 +0200

    starting to get a header

---
+++ src/cmps/user-nav-bar.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="user-nav-bar">
+        <router-link :to="`/user/${loggedInUser._id}`">
+            Account
+        </router-link>
+        <router-link to="/login">Login / Signup</router-link>
+    
+    </section>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            loggedInUser: Object
+        }
+    }
+</script>
