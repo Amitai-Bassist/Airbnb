0ab1fc5a94a7fba2eb29ad253774215e9485da54
Eran Peled <peled2k@gmail.com>
2022-12-01 11:35:25 +0200

    morning maintence

--- src/main.js
+++ src/main.js
@@ -8,12 +8,10 @@ import './assets/styles/setup/_typography.scss';
 import rootCmp from './root-cmp.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-import LitepieDatepicker from 'litepie-datepicker'
 
 const app = createApp(rootCmp);
 app.use(router);
 app.use(store);
 app.use(ElementPlus);
-app.use(LitepieDatepicker)
 
 app.mount('#app');
