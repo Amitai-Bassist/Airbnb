e5e5d08fab331d4ebde45f66c6bd80dd8b308fd4
Eran Peled <peled2k@gmail.com>
2022-12-02 00:51:31 +0200

    add heart

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -13,7 +13,9 @@
         &#160;
         &#160;
         &#160;
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 2; overflow: visible;"><path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path></svg>
+        <svg class="heart" :class="[isSaved ? 'red' : 'white']" @click="toggleWishlist" 
+          viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 24px; width: 24px; stroke:#ffffff; stroke-width: 2; overflow: visible;"><path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path></svg>
+          <path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path>
         &#160;
         <p class="under-line">Save</p>
       </div>
@@ -138,6 +140,7 @@ export default {
       // middleDot: &#183;,
       // space: &#160;,
       stay: null,
+      isSaved: false,
       isEditMode: false,
       isReserve: false,
       reservation: {
@@ -197,7 +200,11 @@ export default {
       // console.log('amenities',this.demoAmenities);
       const idx = this.demoAmenities.findIndex(Amenitie=> Amenitie.name === amenitie)
       return idx
-    }
+    },
+    toggleWishlist() {
+        this.isSaved = !this.isSaved;
+    },
+
   },
   computed: {
     formattedDate() {
