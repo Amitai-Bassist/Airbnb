4e17746c05f29be0efcb886d05228b10dcd23e7d
Nufar Maimon <nufar.maimon@gmail.com>
2022-12-05 19:43:19 +0200

    dashboard

--- src/main.js
+++ src/main.js
@@ -10,13 +10,16 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import 'v-calendar/dist/style.css';
 import VCalendar from 'v-calendar';
-import VueClickAway from "vue3-click-away";
+import VueClickAway from 'vue3-click-away';
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 const app = createApp(rootCmp);
 app.use(router);
 app.use(store);
 app.use(ElementPlus);
 app.use(VCalendar, {});
-app.use(VueClickAway)
+app.use(VueClickAway);
+app.component('EasyDataTable', Vue3EasyDataTable);
 
 app.mount('#app');
--- src/views/stay-dashboard.vue
+++ src/views/stay-dashboard.vue
@@ -1,24 +1,92 @@
 <template>
-    <section style="margin-top:200px;">
-      <h1>Dashboard</h1>
-    </section>
-  
+    <section style="margin-top:200px;" class="">
+      <EasyDataTable
+      :headers="headers"
+      :items="items"
     
+      />
+    </section>
 </template>
-  
+
 <script>
-  export default {
-    name: 'dashboard',
-    data() {
-      return {
-
-      }
-    },
-    computed: {
-
-    },
-    methods: {
-        
-    } 
+// import { Header, Item } from "vue3-easy-data-table";
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "Status", value: "status" },
+        { text: "Guests", value: "guests"},
+        { text: "Check-In", value: "checkIn"},
+        { text: "Check-Out", value: "checkOut"},
+        { text: "Booked", value: "booked"},
+        { text: "Listing", value: "listing", sortable: true},
+        { text: "Total Payout", value: "totalPrice", width: 200},
+        { text: "", value: ""},
+    ],
+      items: [
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+        { status: "Approved", guests: 5, checkIn: "07/12/22", checkOut: "11/12/22", booked: "01/12/22", listing: "City View Apartment", totalPrice: "350", hostActions: "Approved"},
+      ],
+    }
+  },
+  created() {
+    var isHost = false;
+    var host = '';
+    var hostActions = '';
+    if(isHost) {
+      host = 'host'
+      hostActions = 'hostActions' 
+    }
+    this.headers = [
+        { text: "Status", value: "status" },
+        { text: "Guests", value: "guests"},
+        { text: "Check-In", value: "checkIn"},
+        { text: "Check-Out", value: "checkOut"},
+        { text: "Booked", value: "booked"},
+        { text: "Listing", value: "listing", sortable: true},
+        { text: "Total Payout", value: "totalPrice", width: 200},
+        { text: host, value: hostActions },
+    ]
+  },
+  methods: {
+    // headerItemClassNameFunction(ev) {
+      // (header: header, index: number): string => {
+      // if (header.value === 'hostActions') return 'is-host';
+      // return '';
+      // };
+    //   this.headers.forEach(header=> {
+    //     if(header.value === 'hostActions') {
+    //       console.log(ev)
+    //       return 'is-host';
+    //     }
+    //     else return ''; 
+    //   })
+    // },
+    // bodyItemClassName() {
+    //   (column: string, index: number): string => {
+    //   if (column === 'hostActions') return 'is-host';
+    //   return '';
+    //   };
+    // }
   }
+}
+
+// :body-item-class-name="bodyItemClassName"
+
 </script>
+
+<style>
+.is-host {
+  display: none;
+}
+</style>
+
+
+
